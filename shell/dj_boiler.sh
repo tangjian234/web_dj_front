94c85a1b618fe95dea1080dea45e0dc3742dac42
tangjian234 <38194994+tangjian234@users.noreply.github.com>
2020-10-16 23:30:29 -0700

    comment

--- shell/dj_boiler.sh
+++ shell/dj_boiler.sh
@@ -3,8 +3,8 @@
 project_name=$0
 app_name=$1
 
-echo project_name
-echo app_name
+echo $project_name
+echo $app_name
 # 1. build virtural env 
 #source ~/.local/bin/virtualenvwrapper.sh
 #mkvirtualenv "{$project_name}_env"
