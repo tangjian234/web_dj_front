b27710957c54638d09896c8fec3f55a847c7e4be
tangjian234 <38194994+tangjian234@users.noreply.github.com>
2020-10-17 00:16:16 -0700

    comment

--- shell/dj_boiler.sh
+++ shell/dj_boiler.sh
@@ -1,4 +1,5 @@
 
+# download_web ; bash ./shell/dj_boiler.sh locallibrary catalog
 
 project_name=$1
 app_name=$2
@@ -14,10 +15,12 @@ django-admin startproject $project_name
 cd $project_name
 python3 manage.py startapp $app_name 
 
+# 3. run 
+python3 manage.py runserver
+
 
 # 4. build virtural env 
 source ~/.local/bin/virtualenvwrapper.sh
 # echo $s
+echo "remember: "
 echo "mkvirtualenv $s"
-
-# 3. 
