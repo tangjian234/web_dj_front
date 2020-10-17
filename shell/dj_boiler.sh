1dcb1e7abaceb9f3714095747e5c39555f894763
tangjian234 <38194994+tangjian234@users.noreply.github.com>
2020-10-16 23:40:34 -0700

    comment

--- shell/dj_boiler.sh
+++ shell/dj_boiler.sh
@@ -6,11 +6,12 @@ app_name=$2
 echo $project_name
 echo $app_name
 echo $project_name"_env"
+
 # 1. build virtural env 
-# source ~/.local/bin/virtualenvwrapper.sh
-# mkvirtualenv "{$project_name}_env"
+source ~/.local/bin/virtualenvwrapper.sh
+mkvirtualenv "{$project_name}_env"
 
 # 2. create project 
-#django-admin startproject $project_name
-#cd $project_name
-#python3 manage.py startapp $app_name 
+django-admin startproject $project_name
+cd $project_name
+python3 manage.py startapp $app_name 
